01686085f2ee3ac55106a636f1435d7bc51c8cf8
Hasan Alper Aydın <hasanalper.aydin@gmail.com>
2023-05-15 15:06:19 +0300

    style the app

--- main.py
+++ main.py
@@ -8,80 +8,116 @@ with open("content.json", "r") as file:
     content = json.load(file)
 
 window = tkinter.Tk()
-window.geometry("650x700")
+window.geometry("640x720")
 window.title("Lucca - Sudoku Solver")
 
-window.rowconfigure(0, minsize=120)
-window.rowconfigure(1)
-window.rowconfigure(2, weight=1)
+window.rowconfigure(0, minsize=115)
+window.rowconfigure(1, weight=1, minsize=5) # Separator
+window.rowconfigure(2)
+window.rowconfigure(3, weight=1, minsize=5) # Separator
+window.rowconfigure(4, weight=1, minsize=480)
 
 window.columnconfigure(0, weight=1, minsize=480)
-window.columnconfigure(1)
+window.columnconfigure(1, weight=1, minsize=5) # Separator
+window.columnconfigure(2)
 
 window.bind("<q>", lambda e: window.destroy())
 window.bind("<o>", lambda e: upload_file())
 window.bind("<c>", lambda e: open_camera())
 
+style = ttk.Style()
+style.configure("TLabel", font=("Helvetica", 17), foreground="#444")
+style.configure("TButton", width=7, padding=5)
+style.configure("nav.TButton", width=5)
+
+
 ########## HEADER FRAME ##########
-header_frame = ttk.Frame(window, relief="solid")
-header_frame.grid(row=0, column=0, columnspan=2, sticky="nesw")
+header_frame = ttk.Frame(window)
+header_frame.grid(row=0, column=0, columnspan=3, padx=10, pady=(15, 10), sticky="nesw")
 
 header_var = tkinter.StringVar(value="Home")
-header_label = ttk.Label(header_frame, textvariable=header_var, font=("Helvetica", 24), relief="solid")
-header_label.pack(fill="x", padx=10, pady=10)
+header_label = ttk.Label(header_frame, textvariable=header_var, font=("Helvetica", 28), foreground="#000")
+header_label.pack(fill="x")
 
-description_var = tkinter.StringVar(value="Select an image of a Sudoku image.")
-description_label = ttk.Label(header_frame, textvariable=description_var, font=("Helvetica", 16), wraplength=550, relief="solid")
-description_label.pack(fill="x", padx=10, pady=(0, 10))
+description_var = tkinter.StringVar(value="Start by uploading an image of a Sudoku.")
+description_label = ttk.Label(header_frame, textvariable=description_var, wraplength=600)
+description_label.pack(fill="x", pady=(10, 0))
+
+
+##### HEADER SEPARATOR FRAME #####
+header_separator_frame = ttk.Frame(window)
+header_separator_frame.grid(row=1, column=0, columnspan=3, padx=10, sticky="nesw")
+
+header_separator = ttk.Separator(header_separator_frame, orient='horizontal')
+header_separator.pack(side="bottom", fill="x")
 
 
 ########### PATH FRAME ###########
-path_frame = ttk.Frame(window, relief="solid")
-path_frame.grid(row=1, column=0, columnspan=2, sticky="nesw")
+path_frame = ttk.Frame(window)
+path_frame.grid(row=2, column=0, columnspan=3, padx=10, pady=10, sticky="nesw")
 
-upload_button = ttk.Button(path_frame, text="Open", command=lambda: upload_file())
-upload_button.grid(row=0, column=0, padx=10, pady=5)
+upload_button = ttk.Button(path_frame, text="Open...", command=lambda: upload_file())
+upload_button.grid(row=0, column=0)
 
-camera_button = ttk.Button(path_frame, text="Camera", command=lambda: open_camera())
-camera_button.grid(row=1, column=0, padx=10, pady=(0,5))
+camera_button = ttk.Button(path_frame, text="Camera...", command=lambda: open_camera())
+camera_button.grid(row=1, column=0)
 
 path = tkinter.StringVar(value="No Image Selected")
-upload_label = ttk.Label(path_frame, textvariable=path, font=("Helvetica", 16), relief="solid")
-upload_label.grid(row=0, column=1, rowspan=2, sticky="nesw", padx=10, pady=10)
+upload_label = ttk.Label(path_frame, textvariable=path, font=("Helvetica", 15))
+upload_label.grid(row=0, column=1, rowspan=2, padx=15, sticky="nesw")
+
+
+###### PATH SEPARATOR FRAME ####3#
+path_separator_frame = ttk.Frame(window)
+path_separator_frame.grid(row=3, column=0, columnspan=3, padx=10, sticky="nesw")
+
+path_separator = ttk.Separator(path_separator_frame, orient='horizontal')
+path_separator.pack(fill="x")
 
 
 ########## RESULT FRAME ##########
-result_frame = ttk.Frame(window, relief="solid")
-result_frame.grid(row=2, column=0, sticky="nesw")
+result_frame = ttk.Frame(window)
+result_frame.grid(row=4, column=0, padx=10, pady=10, sticky="nesw")
 
 image = Image.open("Screenshots/no-image.jpg")
 image.thumbnail((450, 450))
 tk_image = ImageTk.PhotoImage(image)
-image_label = ttk.Label(result_frame, image=tk_image, relief="solid")
+image_label = ttk.Label(result_frame, image=tk_image)
 image_label.pack(padx=10, pady=10)
 
 
+### NAVIGATION SEPARATOR FRAME ###
+navigation_separator_frame = ttk.Frame(window)
+navigation_separator_frame.grid(row=4, column=1, pady=10, sticky="nesw")
+
+navigation_separator = ttk.Separator(navigation_separator_frame, orient='vertical')
+navigation_separator.pack(side="left", fill="y")
+
+
 ######## NAVIGATION FRAME ########
-navigation_frame = ttk.Frame(window, relief="solid")
-navigation_frame.grid(row=2, column=1, sticky="nesw")
+navigation_frame = ttk.Frame(window)
+navigation_frame.grid(row=4, column=2, padx=10, pady=10, sticky="nesw")
 
-home_button = ttk.Button(navigation_frame, text="Home", state="disabled", command=lambda: home_image())
-home_button.pack(padx=10, pady=10)
+home_button = ttk.Button(navigation_frame, text="Home", state="disabled", style="nav.TButton", command=lambda: home_image())
+home_button.pack()
 
-back_button = ttk.Button(navigation_frame, text="Back", state="disabled", command=lambda: back_image())
-back_button.pack(padx=10, pady=(0, 10))
+back_button = ttk.Button(navigation_frame, text="Back", state="disabled", style="nav.TButton", command=lambda: back_image())
+back_button.pack()
 
-next_button = ttk.Button(navigation_frame, text="Next", state="disabled", command=lambda: next_image())
-next_button.pack(padx=10, pady=(0, 10))
+next_button = ttk.Button(navigation_frame, text="Next", state="disabled", style="nav.TButton", command=lambda: next_image())
+next_button.pack()
 
-skip_button = ttk.Button(navigation_frame, text="Skip", state="disabled", command=lambda: skip_image())
-skip_button.pack(padx=10, pady=(0, 10))
+skip_button = ttk.Button(navigation_frame, text="Skip", state="disabled", style="nav.TButton", command=lambda: skip_image())
+skip_button.pack()
 
-save_button = ttk.Button(navigation_frame, text="Save Image", state="disabled", command=lambda: save_image())
-save_button.pack(padx=10, pady=10)
+save_separator = ttk.Separator(navigation_frame, orient='horizontal')
+save_separator.pack(side="top", fill="x", pady=10)
 
-shortcut_button = ttk.Button(navigation_frame, text="Shortcuts", command=lambda: open_shortcuts())
-shortcut_button.pack(side="bottom", padx=10, pady=10)
+save_button = ttk.Button(navigation_frame, text="Save...", state="disabled", style="nav.TButton", command=lambda: save_image())
+save_button.pack()
+
+shortcut_button = ttk.Button(navigation_frame, text="Shortcuts...", width=8, command=lambda: open_shortcuts())
+shortcut_button.pack(side="bottom")
 
 
 index = 1
@@ -224,18 +260,18 @@ def save_image():
 
 def open_shortcuts():
     menu = tkinter.Toplevel(window)
-    menu.geometry("300x200")
+    menu.geometry("400x270")
     menu.title("Keyboard Shortcuts")
     menu.bind("<q>", lambda e: menu.destroy())
 
-    ttk.Label(menu, text = "<q> Quit the app.").pack()
-    ttk.Label(menu, text = "<c> Open the camera.").pack()
-    ttk.Label(menu, text = "<o> Open the file explorer.").pack()
-    ttk.Label(menu, text = "<Up> Go to the original image.").pack()
-    ttk.Label(menu, text = "<Down> Go to the result image.").pack()
-    ttk.Label(menu, text = "<Left> Go to the previous image.").pack()
-    ttk.Label(menu, text = "<Right> Go to the next image.").pack()
-    ttk.Label(menu, text = "<s> Save the current image.").pack()
+    ttk.Label(menu, text="<q> Quit the app.").pack(pady=(20, 2))
+    ttk.Label(menu, text="<c> Open the camera.").pack(pady=2)
+    ttk.Label(menu, text="<o> Open the file explorer.").pack(pady=2)
+    ttk.Label(menu, text="<Up> Go to the original image.").pack(pady=2)
+    ttk.Label(menu, text="<Down> Go to the result image.").pack(pady=2)
+    ttk.Label(menu, text="<Left> Go to the previous image.").pack(pady=2)
+    ttk.Label(menu, text="<Right> Go to the next image.").pack(pady=2)
+    ttk.Label(menu, text="<s> Save the current image.").pack(pady=2)
 
     menu.mainloop()  
 
